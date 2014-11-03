4d03aa1f83c0826b54d7d1a34faeb61ae259545d
Androyen <androyen@aol.com>
2014-11-03 15:54:49 -0600

    Add Toast Message. Create DetailActivity

--- app/src/main/java/com/androyen/sunshine/ForecastFragment.java
+++ app/src/main/java/com/androyen/sunshine/ForecastFragment.java
@@ -12,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -71,6 +73,13 @@ public  class ForecastFragment extends Fragment {
         mForecastAdapter = new ArrayAdapter<String>(getActivity(), R.layout.list_item_forecast, R.id.list_item_forecast_textview, weekForecast);
         ListView listView = (ListView)rootView.findViewById(R.id.listView_forecast);
         listView.setAdapter(mForecastAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(), mForecastAdapter.getItem(position), Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
