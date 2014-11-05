a8aeb86f7fe0e36608270d9b80dc1f1d1c0c69d6
Androyen <androyen@aol.com>
2014-11-05 13:59:43 -0600

    Implement LocationEntry SQL statement

--- app/src/main/java/com/androyen/sunshine/Data/WeatherDbHelper.java
+++ app/src/main/java/com/androyen/sunshine/Data/WeatherDbHelper.java
@@ -3,6 +3,7 @@ package com.androyen.sunshine.Data;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import com.androyen.sunshine.Data.WeatherContract.LocationEntry;
 
 /**
  * Created by rnguyen on 11/4/14.
@@ -24,6 +25,16 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
         //location setting, the city name, and the latitude and longtitude
 
         //TBD
+        //LocationEntry SQL statements
+        final String SQL_CREATE_LOCATION_TABLE = "CREATE TABLE " + LocationEntry.TABLE_NAME + " (" +
+        LocationEntry._ID + "INTEGER PRIMARY KEY," +
+        LocationEntry.COLUMN_LOCATION_SETTING + " TEXT UNIQUE NOT NULL, " +
+        LocationEntry.COLUMN_CITY_NAME + " TEXT NOT NULL, " +
+        LocationEntry.COLUMN_COORD_LAT + " REAL NOT NULL, " +
+        LocationEntry.COLUMN_COORD_LONG + " REAL NOT NULL, " +
+        "UNIQUE (" + LocationEntry.COLUMN_LOCATION_SETTING + ") ON CONFLICT IGNORE" +
+         " );";
+
 
         final String SQL_CREATE_WEATHER_TABLE = "CREATE TABLE " + WeatherContract.WeatherEntry.TABLE_NAME + " (" +
 
@@ -58,6 +69,8 @@ public class WeatherDbHelper extends SQLiteOpenHelper {
          " UNIQUE (" + WeatherContract.WeatherEntry.COLUMN_DATETEXT + ", " +
          WeatherContract.WeatherEntry.COLUMN_LOC_KEY + ") ON CONFLICT REPLACE);";
 
+        db.execSQL(SQL_CREATE_LOCATION_TABLE);
+        db.execSQL(SQL_CREATE_WEATHER_TABLE);
 
     }
 
